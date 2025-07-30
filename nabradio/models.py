7ab8b78c1b3f8fa05fab6bea72e862c0b43a8968
Alex <a_mnt@outlook.fr>
2025-07-30 13:25:52 +0200

    nabradio corrections actions - models.py

--- nabradio/models.py
+++ nabradio/models.py
@@ -3,9 +3,17 @@ from django.db import models
 from nabcommon import singleton_model
 
 class Config(singleton_model.SingletonModel):
-    # Champ existant
     streaming_url = models.TextField(null=True, default="")
     json_data_base = models.TextField(null=True, default="")
-    
-    # Nouveau champ pour sauvegarder plusieurs URL
     radio_urls = models.JSONField(null=True, blank=True, default=list)
+
+
+class WebRadio(models.Model):
+    url = models.URLField()
+    position = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.url} (#{self.position})"
+
+    class Meta:
+        ordering = ['position']
